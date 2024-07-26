f6a92ec33d2f066a79c411299d00ac4890b77b2b
Safayat Khan Rohan <safayatkhanrohan@gmail.com>
2024-07-26 23:43:53 +0600

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const { Client, IntentsBitField } = require("discord.js");
+const app = require("express")();
+
+require("dotenv").config();
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("messageCreate", (msg) => {
+  if (msg.author.bot) return;
+  msg.reply("Thanks for the message !");
+});
+
+client.login(process.env.TOKEN);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`);
+});
